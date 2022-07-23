73f39f590e32cdbba79ef672fbfd123945774006
Martin Ruggeri <martin.ruggeri@everis.nttdata.com>
2022-07-22 23:24:46 -0300

    Update intro node 2

--- 4- Node JS/intro_nodejs_2/src/app.js
+++ 4- Node JS/intro_nodejs_2/src/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 
+// SETTING
 app.use(morgan('common'));
-const PORT = 8080;
+app.set('view', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs')
 
-app.listen(PORT , () =>{
+require('dotenv').config({path:'../.env'});
+const port = process.env.PORT || 8080;
+
+
+// SERVER
+app.listen(port , () =>{
     console.log(`Servidor RUN`);
-    console.log(`htpp://localhost:${PORT}/`);
+    console.log(`htpp://localhost:${port}/`);
 });
 
+
 // ROUTES
-app.use(require('./route/index.route'));
+app.get('/', (request, response) => {
+    response.render('index.ejs');
+});
+
+//app.use(require('./route/index.route'));
 app.use(require('./route/user.route'));
